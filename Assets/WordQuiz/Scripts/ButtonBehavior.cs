37209933956d95c2143d23644d93319fc6921612
adeshvinod <vinodadesh@gmail.com>
2022-09-29 14:11:44 +0530

    added the main menu scene

---
+++ Assets/WordQuiz/Scripts/ButtonBehavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LoadScene(string scene_name)
+    {
+        SceneManager.LoadScene(scene_name);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
